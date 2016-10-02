8b8b70ea6fedd823152730c51254f6b19d0d3188
Johan Abbors <jabbors@gmail.com>
2016-10-02 13:29:51 +0300

    Add script to fetch, parse and insert rates

---
+++ fetch-parse-insert.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+fetch_page() {
+    maturity=$1
+    page="/euribor-rate-${maturity}.asp"
+    file="/tmp/${page}"
+    url="http://www.euribor-rates.eu/${page}"
+    if [ ! -f ${file} ]
+    then
+        echo "fetching page ${url}"
+        curl ${url} -o ${file}
+    fi
+}
+
+parse_date() {
+    maturity=$1
+    page="/euribor-rate-${maturity}.asp"
+    file="/tmp/${page}"
+    cat ${file} | grep "TABLE" -A 8 -m 1 | grep -o -e "[0-9]\{2\}-[0-9]\{2\}-[0-9]\{4\}"
+}
+
+parse_rate() {
+    maturity=$1
+    page="/euribor-rate-${maturity}.asp"
+    file="/tmp/${page}"
+    cat ${file} | grep "TABLE" -A 8 -m 1 | grep -o -e "-\?[0-9]\{1,2\}\.[0-9]\{3\}"
+}
+
+last_inserted_time() {
+    maturity=$1
+    ts=$(influx -format csv -execute "SELECT * FROM euribor.week.rates WHERE maturity='${maturity}' ORDER BY time DESC LIMIT 1" | tail -n 1 | cut -d ',' -f2)
+    date -d @$(( ts / 1000000000 )) +%m-%d-%Y
+}
+
+insert_data() {
+    maturity=$1
+    date=$2
+    rate=$3
+    ts=$(date_to_timestamp_nano ${date})
+    echo "inserting data: INSERT rates,maturity=${maturity} value=${rate} ${ts} (${date})"
+    influx -execute "INSERT INTO euribor.week rates,maturity=${maturity} value=${rate} ${ts}"
+}
+
+date_to_timestamp_nano() {
+    date=$1
+    year=$(echo ${date} | cut -d '-' -f3)
+    month=$(echo ${date} | cut -d '-' -f1)
+    day=$(echo ${date} | cut -d '-' -f2)
+    time="12:00:00"
+    date -d "${year}${month}${day} ${time}" -u +%s%N
+}
+
+rm -f /tmp/euribor*
+# As of November 1st 2013 the number of Euribor rates was reduced to 8 (1-2 weeks, 1, 2, 3, 6, 9 and 12 months).
+for maturity in 1-week 2-weeks 1-month 2-months 3-months 6-months 9-months 12-months
+do
+    echo "processing maturity ${maturity}"
+    fetch_page ${maturity}
+    date=$(parse_date ${maturity})
+    if [ ${#date} -ne 10 ]
+    then
+        echo "no date found, something might be wrong"
+        continue
+    fi
+    rate=$(parse_rate $maturity)
+    last_inserted=$(last_inserted_time ${maturity})
+    if [ "${last_inserted}" == "${date}" ]
+    then
+        echo "data for ${date} already inserted"
+        continue
+    fi
+    insert_data $maturity $date $rate
+done
