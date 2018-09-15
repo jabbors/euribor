fe3dbcce82a4a7dbf2b3d113cddb4358cb79c032
Johan Abbors <jabbors@gmail.com>
2018-09-15 22:39:47 +0200

    Refactor move rate logic into its own file

--- gorates/main.go
+++ gorates/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
-	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -18,42 +15,6 @@ const (
 )
 
 var version string
-
-type rateSlice []rate
-
-func (rs rateSlice) Values() string {
-	str := "["
-	for i, r := range rs {
-		str += "["
-		str += strconv.FormatInt(r.Date.Unix()*1000, 10) // in milli seconds
-		str += ","
-		str += strconv.FormatFloat(r.Value, 'g', 4, 64)
-		str += "]"
-		if i != len(rs)-1 {
-			str += ","
-		}
-	}
-	str += "]"
-
-	return str
-}
-
-type rate struct {
-	Date  time.Time `json:"date"`
-	Value float64   `json:"value"`
-}
-
-// MarshalJSON overrides marshal, prints shorter date string
-func (r *rate) MarshalJSON() ([]byte, error) {
-	type alias rate
-	return json.Marshal(&struct {
-		Date string `json:"date"`
-		*alias
-	}{
-		Date:  r.Date.Format("2006-01-02"),
-		alias: (*alias)(r),
-	})
-}
 
 var maturities = []string{"1w", "2w", "1m", "2m", "3m", "6m", "9m", "12m"}
 var retentions = []string{"week", "month", "three_months", "six_months", "year", "two_years", "six_years"}
