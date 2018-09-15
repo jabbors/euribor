13a3119710ec46480f0371595671523ea2d07969
Johan Abbors <jabbors@gmail.com>
2018-09-15 22:31:49 +0200

    Refactor move cache logic into its own file

---
+++ gorates/cache.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"reflect"
+	"strconv"
+	"time"
+)
+
+var (
+	lastRefresh  time.Time
+	historyCache map[string][]rate
+	influxCache  map[string]map[string][]rate
+)
+
+// runs in go routine and takes care of refreshing the cache
+func refreshCache() {
+	historyCache = make(map[string][]rate)
+	influxCache = make(map[string]map[string][]rate)
+
+	for {
+		if time.Since(lastRefresh) < time.Hour*24 {
+			time.Sleep(time.Minute)
+			continue
+		}
+
+		// refresh
+		log.Println("[cache]: refreshing history cache")
+		for _, maturity := range maturities {
+			// TODO: make path to files configureable
+			file := fmt.Sprintf("%s/euribor-rates-%s.csv", historyPath, maturity)
+			historyCache[maturity] = parseFile(file)
+		}
+		log.Println("[cache]: refreshing influx cache")
+		for _, retention := range retentions {
+			results := queryInflux(retention)
+			cache := make(map[string][]rate)
+			// fmt.Println(results.Values)
+			// fmt.Println(reflect.TypeOf(results.Values).Kind())
+			for _, value := range results.Values {
+				// fmt.Println(value, reflect.TypeOf(value).Kind(), reflect.TypeOf(value))
+				m, r, err := transformInfluxValueToRate(reflect.ValueOf(value).Interface().([]interface{}))
+				if err != nil {
+					log.Println("[cache]: error converting influx value to rate")
+					continue
+				}
+				if rates, ok := cache[m]; ok {
+					rates = append(rates, r)
+					cache[m] = rates
+				} else {
+					cache[m] = []rate{r}
+				}
+			}
+			influxCache[retention] = cache
+		}
+		log.Println("[cache]: refresh completed")
+
+		lastRefresh = time.Now()
+	}
+}
+
+func parseFile(file string) []rate {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Println(err)
+		return []rate{}
+	}
+
+	r := csv.NewReader(f)
+	r.Comment = '#'
+	r.FieldsPerRecord = 2
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Println(err)
+		return []rate{}
+	}
+
+	rates := []rate{}
+	for _, record := range records {
+		date, err := time.ParseInLocation(timeFormat, record[0], time.UTC)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		value, err := strconv.ParseFloat(record[1], 64)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		rates = append(rates, rate{date, value})
+	}
+
+	return rates
+}
--- gorates/main.go
+++ gorates/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"encoding/csv"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"reflect"
 	"strconv"
 	"time"
 
@@ -58,7 +55,6 @@ func (r *rate) MarshalJSON() ([]byte, error) {
 	})
 }
 
-var lastRefresh time.Time
 var maturities = []string{"1w", "2w", "1m", "2m", "3m", "6m", "9m", "12m"}
 var retentions = []string{"week", "month", "three_months", "six_months", "year", "two_years", "six_years"}
 var retentionMap = map[string]string{
@@ -72,89 +68,6 @@ var retentionMap = map[string]string{
 }
 var webRoot string
 var historyPath string
-var historyCache map[string][]rate
-var influxCache map[string]map[string][]rate
-
-// runs in go routine and takes care of refreshing the cache
-func refreshCache() {
-	historyCache = make(map[string][]rate)
-	influxCache = make(map[string]map[string][]rate)
-
-	for {
-		if time.Since(lastRefresh) < time.Hour*24 {
-			time.Sleep(time.Minute)
-			continue
-		}
-
-		// refresh
-		log.Println("[cache]: refreshing history cache")
-		for _, maturity := range maturities {
-			// TODO: make path to files configureable
-			file := fmt.Sprintf("%s/euribor-rates-%s.csv", historyPath, maturity)
-			historyCache[maturity] = parseFile(file)
-		}
-		log.Println("[cache]: refreshing influx cache")
-		for _, retention := range retentions {
-			results := queryInflux(retention)
-			cache := make(map[string][]rate)
-			// fmt.Println(results.Values)
-			// fmt.Println(reflect.TypeOf(results.Values).Kind())
-			for _, value := range results.Values {
-				// fmt.Println(value, reflect.TypeOf(value).Kind(), reflect.TypeOf(value))
-				m, r, err := transformInfluxValueToRate(reflect.ValueOf(value).Interface().([]interface{}))
-				if err != nil {
-					log.Println("[cache]: error converting influx value to rate")
-					continue
-				}
-				if rates, ok := cache[m]; ok {
-					rates = append(rates, r)
-					cache[m] = rates
-				} else {
-					cache[m] = []rate{r}
-				}
-			}
-			influxCache[retention] = cache
-		}
-		log.Println("[cache]: refresh completed")
-
-		lastRefresh = time.Now()
-	}
-}
-
-func parseFile(file string) []rate {
-	f, err := os.Open(file)
-	if err != nil {
-		log.Println(err)
-		return []rate{}
-	}
-
-	r := csv.NewReader(f)
-	r.Comment = '#'
-	r.FieldsPerRecord = 2
-
-	records, err := r.ReadAll()
-	if err != nil {
-		log.Println(err)
-		return []rate{}
-	}
-
-	rates := []rate{}
-	for _, record := range records {
-		date, err := time.ParseInLocation(timeFormat, record[0], time.UTC)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		value, err := strconv.ParseFloat(record[1], 64)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		rates = append(rates, rate{date, value})
-	}
-
-	return rates
-}
 
 func isValidRetention(r string) bool {
 	if _, ok := retentionMap[r]; ok {
