a6a67d13f644785c9e52e08151895de02893be32
Johan Abbors <jabbors@gmail.com>
2019-03-31 14:23:18 +0300

    Use highstock version 7.0.3

--- gorates/webapp.go
+++ gorates/webapp.go
@@ -11,7 +11,7 @@ const indexPage = `
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Euribor rates</title>
-<script src="https://code.highcharts.com/stock/5.0.7/highstock.js"></script>
+<script src="https://code.highcharts.com/stock/7.0.3/highstock.js"></script>
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="apple-touch-icon" sizes="57x57" href="https://github.com/jabbors/euribor/raw/master/gorates/chart-icon-57x57.png" />
